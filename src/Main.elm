cc4f2968dc132e54a12b316c1db706b9202eac75
TechnoTone <TechnoTone@gmail.com>
2024-04-28 14:07:28 +0100

    restore board image path

--- src/Main.elm
+++ src/Main.elm
@@ -119,7 +119,7 @@ viewScreen : Model -> Html.Html Msg
 viewScreen model =
     case model.screen of
         Board ->
-            viewFullScreenImage "/static/img/board.jpg" model
+            viewFullScreenImage "board.jpg" model
 
         Leaders ->
             viewLeaders
